b68c36a4ff2361e7ebea1c0f1e166a87949ef84a
Seb Insua <seb.insua@r3pi.io>
2017-03-02 15:32:41 +0000

    chore(init): basic logic

---
+++ README.md
@@ -0,0 +1,31 @@
+# `ietf-language-tag-regex`
+> Regular expressions for matching IETF language tags (BCP 47)
+
+Based on this [Stack Overflow comment](http://stackoverflow.com/a/7036171) by George Pollard.
+
+## Install
+
+## Usage
+
+```js
+
+const languageTagRegex = require('ietf-language-tag-regex')
+
+languageTagRegex().test('en-GB') // true
+
+```
+
+## API
+
+### languageTagRegex(options)
+
+Returns a regex for matching IETF language tags.
+
+#### options
+
+##### exact
+
+Type: `boolean`<br>
+Default: `false`
+
+Only match an exact string. Useful with `RegExp#test` to check if a string is a language tag.
---
+++ index.js
@@ -0,0 +1,21 @@
+const createRegExp = require('xregexp')
+
+const regular = '(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang)'
+const irregular = '(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)'
+const grandfathered = '(?<grandfathered>' + irregular + '|' + regular + ')'
+const privateUse = '(?<privateUse>x(-[A-Za-z0-9]{1,8})+)'
+const privateUse2 = '(?<privateUse2>x(-[A-Za-z0-9]{1,8})+)'
+const singleton = '[0-9A-WY-Za-wy-z]'
+const extension = '(?<extension>' + singleton + '(-[A-Za-z0-9]{2,8})+)'
+const variant = '(?<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3})'
+const region = '(?<region>[A-Za-z]{2}|[0-9]{3})'
+const script = '(?<script>[A-Za-z]{4})'
+const extlang = '(?<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2})'
+const language = '(?<language>([A-Za-z]{2,3}(-' + extlang + ')?)|[A-Za-z]{4}|[A-Za-z]{5,8})'
+const langtag = '(' + language + '(-' + script + ')?' + '(-' + region + ')?' + '(-' + variant + ')*' + '(-' + extension + ')*' + '(-' + privateUse + ')?' + ')'
+const languageTag = '^(' + grandfathered + '|' + langtag + '|' + privateUse2 + ')$'
+
+const createLanguageTagRegex = ({ exact = false }) =>
+  createRegExp(languageTag, exact ? '' : 'g')
+
+module.exports = createLanguageTagRegex
